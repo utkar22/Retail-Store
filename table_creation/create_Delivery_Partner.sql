bcbb736c622b14f89d32ccb76f361b4f46fec962
utkar22 <92251285+utkar22@users.noreply.github.com>
2022-07-13 01:12:45 +0530

    Create create_Delivery_Partner.sql

---
+++ table_creation/create_Delivery_Partner.sql
@@ -0,0 +1,17 @@
+create table Delivery_Partner(
+	delivery_partner_id int PRIMARY KEY,
+	account_id int UNIQUE NOT NULL,
+	first_name varchar(30) NOT NULL,
+	last_name varchar(30),
+	age int,
+	phone_number varchar(15) NOT NULL,
+	house_number varchar(20) NOT NULL,
+	street varchar(40) NOT NULL,
+	city varchar(20) NOT NULL,
+	state varchar(20) NOT NULL,
+	country varchar(20) NOT NULL,
+
+	FOREIGN KEY (account_id) REFERENCES Account(account_id) ON DELETE CASCADE,
+	CHECK (age>=18)
+	)
+;
